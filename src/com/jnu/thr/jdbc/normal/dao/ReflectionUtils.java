7daa697a1392b1d130fcbdf1482c167a1bfc749d
LeasureWang <leasure_wang@outlook.com>
2017-08-07 23:07:03 +0800

    Update ReflectionUtils.java
    7_Aug update -- delete some CHN words

--- src/com/jnu/thr/jdbc/normal/dao/ReflectionUtils.java
+++ src/com/jnu/thr/jdbc/normal/dao/ReflectionUtils.java
@@ -7,22 +7,9 @@ import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-/**
- * 反射的 Utils 函数集合
- * 提供访问私有变量, 获取泛型类型 Class, 提取集合中元素属性等 Utils 函数
- * @author Administrator
- *
- */
+
 public class ReflectionUtils {
 
-	
-	/**
-	 * 通过反射, 获得定义 Class 时声明的父类的泛型参数的类型
-	 * 如: public EmployeeDao extends BaseDao<Employee, String>
-	 * @param clazz
-	 * @param index
-	 * @return
-	 */
 	@SuppressWarnings("unchecked")
 	public static Class getSuperClassGenricType(Class clazz, int index){
 		Type genType = clazz.getGenericSuperclass();
@@ -44,25 +31,11 @@ public class ReflectionUtils {
 		return (Class) params[index];
 	}
 	
-	/**
-	 * 通过反射, 获得 Class 定义中声明的父类的泛型参数类型
-	 * 如: public EmployeeDao extends BaseDao<Employee, String>
-	 * @param <T>
-	 * @param clazz
-	 * @return
-	 */
 	@SuppressWarnings("unchecked")
 	public static<T> Class<T> getSuperGenericType(Class clazz){
 		return getSuperClassGenricType(clazz, 0);
 	}
 	
-	/**
-	 * 循环向上转型, 获取对象的 DeclaredMethod
-	 * @param object
-	 * @param methodName
-	 * @param parameterTypes
-	 * @return
-	 */
 	public static Method getDeclaredMethod(Object object, String methodName, Class<?>[] parameterTypes){
 		
 		for(Class<?> superClass = object.getClass(); superClass != Object.class; superClass = superClass.getSuperclass()){
@@ -70,52 +43,32 @@ public class ReflectionUtils {
 				//superClass.getMethod(methodName, parameterTypes);
 				return superClass.getDeclaredMethod(methodName, parameterTypes);
 			} catch (NoSuchMethodException e) {
-				//Method 不在当前类定义, 继续向上转型
+				
 			}
 			//..
 		}
 		
 		return null;
 	}
-	
-	/**
-	 * 使 filed 变为可访问
-	 * @param field
-	 */
+
 	public static void makeAccessible(Field field){
 		if(!Modifier.isPublic(field.getModifiers())){
 			field.setAccessible(true);
 		}
 	}
 	
-	/**
-	 * 循环向上转型, 获取对象的 DeclaredField
-	 * @param object
-	 * @param filedName
-	 * @return
-	 */
 	public static Field getDeclaredField(Object object, String filedName){
 		
 		for(Class<?> superClass = object.getClass(); superClass != Object.class; superClass = superClass.getSuperclass()){
 			try {
 				return superClass.getDeclaredField(filedName);
 			} catch (NoSuchFieldException e) {
-				//Field 不在当前类定义, 继续向上转型
+				
 			}
 		}
 		return null;
 	}
 	
-	/**
-	 * 直接调用对象方法, 而忽略修饰符(private, protected)
-	 * @param object
-	 * @param methodName
-	 * @param parameterTypes
-	 * @param parameters
-	 * @return
-	 * @throws InvocationTargetException 
-	 * @throws IllegalArgumentException 
-	 */
 	public static Object invokeMethod(Object object, String methodName, Class<?> [] parameterTypes,
 			Object [] parameters) throws InvocationTargetException{
 		
@@ -130,18 +83,12 @@ public class ReflectionUtils {
 		try {
 			return method.invoke(object, parameters);
 		} catch(IllegalAccessException e) {
-			System.out.println("不可能抛出的异常");
+			System.out.println("涓嶅彲鑳芥姏鍑虹殑寮傚父");
 		} 
 		
 		return null;
 	}
-	
-	/**
-	 * 直接设置对象属性值, 忽略 private/protected 修饰符, 也不经过 setter
-	 * @param object
-	 * @param fieldName
-	 * @param value
-	 */
+
 	public static void setFieldValue(Object object, String fieldName, Object value){
 		Field field = getDeclaredField(object, fieldName);
 		
@@ -153,16 +100,10 @@ public class ReflectionUtils {
 		try {
 			field.set(object, value);
 		} catch (IllegalAccessException e) {
-			System.out.println("不可能抛出的异常");
+			System.out.println("涓嶅彲鑳芥姏鍑虹殑寮傚父");
 		}
 	}
 	
-	/**
-	 * 直接读取对象的属性值, 忽略 private/protected 修饰符, 也不经过 getter
-	 * @param object
-	 * @param fieldName
-	 * @return
-	 */
 	public static Object getFieldValue(Object object, String fieldName){
 		Field field = getDeclaredField(object, fieldName);
 		
@@ -176,7 +117,7 @@ public class ReflectionUtils {
 		try {
 			result = field.get(object);
 		} catch (IllegalAccessException e) {
-			System.out.println("不可能抛出的异常");
+			System.out.println("涓嶅彲鑳芥姏鍑虹殑寮傚父");
 		}
 		
 		return result;
